1ed6f650a47da654d0859ef55e7ba21de6180ec2
Jude <jude@jude7230.local>
2022-07-13 10:42:27 +0900

    api 호출 https modify

--- src/store/movie.js
+++ src/store/movie.js
@@ -107,8 +107,8 @@ function _fetchMovie(payload) {
   const OMDB_API_KEY = '7035c60c'
   // const OMDB_API_KEY = '700bc90f'
   const url = id
-    ? `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
 
   return new Promise((resolve, reject) => {
     axios.get(url)
